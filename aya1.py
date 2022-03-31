7971e3ba971f521a0be44e1b9013eee4f7b71316
engayamamdouh <101137651+engayamamdouh@users.noreply.github.com>
2022-03-31 20:39:00 +0200

    Add files via upload

---
+++ aya1.py
@@ -0,0 +1,20 @@
+def rect(h,w):
+    return 2*(h+w)
+premiter=rect(2,5)
+print(premiter)
+def tri(a,b,c):
+    return a+b+c
+result=tri(2,4,5)
+print(result)
+def squa(d):
+    return 4*d
+equal=squa(8)
+print(equal)
+def circal(r):
+    return 2*(22/7)*r
+prog=circal(10)
+print(prog)
+def moayan(s):
+    return 4*s
+get=moayan(9)
+print(get)
